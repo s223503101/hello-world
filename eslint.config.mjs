53d2cac25d0a5e451c08e349ab42dbdc9e140f56
s223503101 <s223503101@deakin.edu.au>
2024-10-04 04:56:27 +1000

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,17 @@
 import { ESLint } from "eslint";
 
-export default {
-  parser: "@typescript-eslint/parser",
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended"
-  ],
-  plugins: ["@typescript-eslint"],
-  rules: {
-    // Add your custom rules here
-  }
-};
+export default [
+  {
+    files: ['**/*.ts', '**/*.tsx'],  // Target TypeScript files
+    languageOptions: {
+      parser: '@typescript-eslint/parser',  // Use the TypeScript parser
+    },
+    plugins: {
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),  // Use the ESLint plugin for TypeScript
+    },
+    rules: {
+      // Add your custom rules here
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+  },
+];
